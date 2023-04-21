46e23061be25b409c2031671bc9bbc86ad8f2110
Zegorax <luca@verardo.ch>
2023-04-21 21:52:30 +0200

    Updated 60-svc-rspamd.sh for local_addrs removal

--- rootfs/etc/cont-init.d/60-svc-rspamd.sh
+++ rootfs/etc/cont-init.d/60-svc-rspamd.sh
@@ -28,3 +28,7 @@ with-contenv
 /usr/sbin/rspamd -i -f -u rspamd -g rspamd
 EOL
 chmod +x /etc/services.d/rspamd/run
+
+# Empty the local_addrs array to avoid having Rspamd skip DMARC and SPF checks if the mailserver is running in a container as well (therefore being in a local network).
+# Required since AnonAddy checks the headers injected by Rspamd. See https://github.com/anonaddy/docker/issues/192#issuecomment-1518111988
+sed -i 's/local_addrs.*$/local_addrs=[]/' /etc/rspamd/options.inc
